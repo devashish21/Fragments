92bcf32d8bca4d9606e02ca96633eeb3a58955af
devashish chourey <devashish_chourey@hotmail.com>
2017-04-28 15:58:56 +0530

    FRAGMENT
    FRAGMENT EXAMPLE

---
+++ app/src/main/java/com/med/devashish/fragments/MainActivity.java
@@ -0,0 +1,39 @@
+package com.med.devashish.fragments;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void ChangeFragment(View view){
+        android.app.Fragment fragment;
+
+        if( view == findViewById(R.id.button)) {
+            fragment = new FragmentOne();
+            FragmentManager fm = getFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.fragment_place, fragment);
+            ft.commit();
+        }
+        if( view == findViewById(R.id.button1)) {
+            fragment = new FragmentTwo();
+            FragmentManager fm = getFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.fragment_place, fragment);
+            ft.commit();
+        }
+    }
+}
